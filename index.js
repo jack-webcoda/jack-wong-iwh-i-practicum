b59f16e951c04b1affbe94d872d0284253b31ef9
jack <jack@webcoda.com.au>
2025-05-14 17:37:40 +1000

    WIP

--- index.js
+++ index.js
@@ -11,12 +11,26 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const customObject = 'https://api.hubapi.com/crm/v3/objects/airlines?properties=airline_name,airline_code,country,website';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const response = await axios.get(customObject, { headers });
+        const data = response.data.results;
+        console.log(data);
+        res.render('index', { title: 'Airlines Data', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
